c18257ee11898794e3ffb25b074ea92f2209eb6c
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-07-03 12:43:20 +0600

    remove unnecessary build file

--- src/app/interfaces/book.interface.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface UserInstanceMethod {
-//   updateAvailability(password: Types.ObjectId): void;
-// }
